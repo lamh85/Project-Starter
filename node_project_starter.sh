f80e9b848f1231883621299455731c5f13c9aaec
lamh85 <lamh85@gmail.com>
2021-11-07 18:07:00 -0800

    Add TODO. Make print-out more noticeable.

--- node_project_starter.sh
+++ node_project_starter.sh
@@ -4,6 +4,7 @@ git init
 
 npm init -y
 
+# TODO: Store the string as a variable.
 echo $'
 node_modules
 
@@ -12,5 +13,5 @@ node_modules
 [[ $(node -v) =~ ([0-9\.]+) ]] && NODE_VERSION="${BASH_REMATCH[1]}"
 echo $NODE_VERSION > .nvmrc
 
-echo 'REMINDER ***'
+echo '⚠️⚠️⚠️ REMINDER ⚠️⚠️⚠️'
 echo 'Remember to edit these files: .gitignore, .nvmrc'
